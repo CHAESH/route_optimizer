69c2e470fabe574a84314133b501180f928c4a7d
sanghee chae <sangheechae@sanghees-MacBook-Air.local>
2025-05-26 00:40:51 +0900

    Initial commit

---
+++ config.example.js
@@ -0,0 +1,8 @@
+// 카카오맵 API 설정
+const KAKAO_MAP_CONFIG = {
+    // JavaScript 키를 입력하세요 (JavaScript 앱 키가 아님)
+    API_KEY: 'YOUR_KAKAO_MAPS_JAVASCRIPT_KEY'
+};
+
+// 전역 변수로 내보내기
+window.KAKAO_MAP_CONFIG = KAKAO_MAP_CONFIG; 
